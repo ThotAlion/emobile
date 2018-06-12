d4cd6cf0d7eaf098e1fcdb2bcf02059586657a1f
ThotAlion <thomas.peyruse@gmail.com>
2018-06-12 22:04:39 +0000

    change of the ros install (more simple)

--- install_ros.sh
+++ install_ros.sh
@@ -1,26 +1,17 @@
 #! /bin/bash
 # Derniere distribution stretch
 sudo apt-get update
-sudo apt-get dist-upgrade -y
-sudo apt-get update
-
-# installation du gestionnaire de clés
-sudo apt-get install -y dirmngr
 
 # instalation de ROS distribution kinetic
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install -y python-rosdep python-rosinstall-generator python-wstool python-rosinstall build-essential cmake
+sudo apt-get install python-rosdep python-rosinstall-generator python-wstool python-rosinstall build-essential
 sudo rosdep init
 rosdep update
-mkdir -p ~/ros_catkin_ws
+mkdir ~/ros_catkin_ws
 cd ~/ros_catkin_ws
-rosinstall_generator mobile --rosdistro kinetic --deps --wet-only --tar > kinetic-ros_comm-wet.rosinstall
-wstool init src kinetic-ros_comm-wet.rosinstall
-rosdep install -y --from-paths src --ignore-src --rosdistro kinetic -r --os=debian:stretch
-sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release -j2
+rosinstall_generator ros_comm --rosdistro kinetic --deps --wet-only --tar > kinetic-ros_comm-wet.rosinstall
+wstool init -j8 src kinetic-ros_comm-wet.rosinstall
+rosdep install --from-paths src --ignore-src --rosdistro kinetic -y
+./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release
 cd ..
 cd ros_catkin_ws/devel_isolated/
 source setup.bash
